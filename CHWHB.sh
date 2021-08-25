330bbe51ee61786d44b19f897bc7951c64ef4418
Hermit <hermit@cryptolingus.net>
2021-08-25 14:37:24 -0400

    Update to logging, added Ghidra installation
    Added install of Ghidra and required dependencies, additional commenting, consistent logging, and cleaned up to add apt upgrade option at start of script

--- CHWHB.sh
+++ CHWHB.sh
@@ -3,13 +3,15 @@
 ##########################################
 # Program:    CHWHB (Create Hardware Hacking Box)
 # Author:     Brian Mork (Hermit)
-# Version:    0.1
-# Modified:   2021-08-04
+# Version:    0.1.2          - Added AVR installs and Ghidra
+# Modified:   2021-08-25
 ##########################################
 
 ######################################################################################
 ###                             Helper Functions                                   ###
 ######################################################################################
+
+LOGFILE=/root/chwhb.log
 
 ################
 # Function:   showError
@@ -18,7 +20,7 @@
 # Returns:    Nothing
 ################
 showError() {
-  echo " [!] " $1
+  echo " [!] " $1 | tee -a ${LOGFILE}
   exit 1
 }
 
@@ -29,7 +31,7 @@ showError() {
 # Returns:    Nothing
 ################
 showWarning() {
-  echo " [W] " $1
+  echo " [W] " $1 | tee -a ${LOGFILE}
 }
 
 ################
@@ -39,7 +41,7 @@ showWarning() {
 # Returns:    Nothing
 ################
 showInfo() {
-  echo " [I] " $1
+  echo " [I] " $1 | tee -a ${LOGFILE}
 }
 
 ################
@@ -181,13 +183,29 @@ fi
 # Get to known starting point
 cd /root
 
+# Add OpenJDK to repositories
+add-apt-repository ppa:openjdk-r/ppa
+
 # Update repositories
 showInfo "Updating repositories..."
-apt update > chwhb.log 2>&1
+apt update > ${LOGFILE} 2>&1
 showInfo "Repositories updated"
+
+# Prepare for non-interactive updates
+export DEBIAN_FRONTEND=noninteractive
+
+# Perform upgrade inline?
+ask "Perform upgrade on all packages before continuing?"
+if [ $RETVAL -eq 1 ]; then
+  showInfo "Performing upgrade"
+  apt -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade >> ${LOGFILE}  2>&1
+fi
 
 # Build core list of things to install
 INSTALLPKGS="android-sdk apktool automake build-essential cmake curl driftnet ettercap-common ettercap-graphical firefoxdriver gcc gcc-arm-none-eabi gdb gdb-multiarch git golang golang-doc hexedit hexdiff libftdi-dev libpci-dev libsmali-java libusb-dev libusb-1.0-0-dev linux-headers-generic linux-headers-$(uname -r) m4 meld minicom nasm nmap openjdk-14-jre picocom python2 python3 python3-dev python3-pip python3-selenium qunit-selenium screen tcptrace tmux tshark wget wireshark xxd"
+
+# Add Ghidra pre-requisites
+INSTALLPKGS="$INSTALLPKGS openjdk-11-jdk openjdk-11-jre-headless"
 
 # Add Arduino-specific items
 INSTALLPKGS="$INSTALLPKGS arduino arduino-core arduino-mk arduino-builder arduino-mighty-1284p"
@@ -214,91 +232,120 @@ INSTALLPKGS="$INSTALLPKGS aircrack-ng airgraph-ng kismet kismet-plugins mdk4 wif
 INSTALLPKGS="$INSTALLPKGS libappindicator1 libgconf-2-4 gconf2-common libdbusmenu-gtk4 libc++1 libc++1-10 libc++abi1-10"
 
 # Install packages
-export DEBIAN_FRONTEND=noninteractive
-apt -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install $INSTALLPKGS >> chwhb.log  2>&1
+apt -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install $INSTALLPKGS >> ${LOGFILE}  2>&1
 
 # Install Salae software
 showInfo "Installing Salae"
 SALAEFILE="/usr/local/sbin/salae-logic-analyzer"
-fileExists $SALAEFILE
+fileExists ${SALAEFILE}
 if [ "$ISFILE" == "yes" ]; then
   showInfo "Found Salae Logic Analyzer already installed.  Software will be updated."
-  rm -f $SALAEFILE 
+  rm -f ${SALAEFILE} 
 fi
-wget -O ${SALAEFILE} "https://logic2api.saleae.com/download?os=linux" >> chwhb.log 2>&1
-chmod +x $SALAEFILE
-showInfo "Installed Salae Logic Analyzer to $SALAEFILE"
+wget -O ${SALAEFILE} "https://logic2api.saleae.com/download?os=linux" >> ${LOGFILE} 2>&1
+chmod +x ${SALAEFILE}
+showInfo "Installed Salae Logic Analyzer to ${SALAEFILE}"
 
 # Install Discord
 showInfo "Installing Discord"
-TMPDIR=`mktemp -d`
-wget -O $TMPDIR/discord.deb 'https://discord.com/api/download?platform=linux&format=deb' >> chwhb.log 2>&1
-dpkg -i $TMPDIR/discord.deb >> chwhb.log 2>&1
+DISCORDTMPDIR=`mktemp -d`
+wget -O ${DISCORDTMPDIR}/discord.deb 'https://discord.com/api/download?platform=linux&format=deb' >> ${LOGFILE} 2>&1
+dpkg -i ${DISCORDTMPDIR}/discord.deb >> ${LOGFILE} 2>&1
 showInfo "Installed Discord"
 
 # Install CHIPSEC
 showInfo "Installing CHIPSEC"
-pip install setuptools >> chwhb.log 2>&1
+pip install setuptools >> ${LOGFILE} 2>&1
 purgeIfFound /root/chipsec
-git clone https://github.com/chipsec/chipsec.git >> chwhb.log 2>&1
+git clone https://github.com/chipsec/chipsec.git >> ${LOGFILE} 2>&1
 cd chipsec
-python setup.py build_ext -i >> chwhb.log 2>&1
+python setup.py build_ext -i >> ${LOGFILE} 2>&1
 cd ..
 showInfo "Installed CHIPSEC"
 
 # Install smali
 showInfo "Installing smali"
 purgeIfFound /root/smali
-git clone https://github.com/JesusFreke/smali.git >> chwhb.log 2>&1
+git clone https://github.com/JesusFreke/smali.git >> ${LOGFILE} 2>&1
 cd smali
-./gradlew build >> chwhb.log 2>&1
+./gradlew build >> ${LOGFILE} 2>&1
 cd ..
 showInfo "Installed smali"
 
 # Install Reverse-APK
 showInfo "Installing Reverse-APK"
 purgeIfFound /root/ReverseAPK
-git clone https://github.com/1N3/ReverseAPK.git /root/ReverseAPK >> chwhb.log 2>&1
+git clone https://github.com/1N3/ReverseAPK.git /root/ReverseAPK >> ${LOGFILE} 2>&1
 cd ReverseAPK
-./install >> chwhb.log 2>&1
+./install >> ${LOGFILE} 2>&1
 cd /root
 showInfo "Installed Reverse-APK"
 
 # Install Postman
 showInfo "Installing Postman"
-snap install postman >> chwhb.log 2>&1
+snap install postman >> ${LOGFILE} 2>&1
 showInfo "Installed Postman"
+
+# Install Ghidra
+showInfo "Installing Ghidra"
+GTEMP=`mktemp -d`
+GINSTALLDIR="/usr/local/ghidra"
+purgeIfFound ${GINSTALLDIR}
+GHIDRAFILE="${GTEMP}/ghidra.zip"
+wget -O ${GHIDRAFILE} "https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_10.0.2_build/ghidra_10.0.2_PUBLIC_20210804.zip" >> ${LOGFILE} 2>&1
+cd ${GTEMP}
+unzip ${GHIDRAFILE}
+# Assumption that the directory structure won't change, may need fixed later
+mv ghidra_* ${GINSTALLDIR}
+ln -s ${GINSTALLDIR}/ghidraRun /usr/local/bin/ghidra
+rm -Rf ${GTEMP}
+cd /root
+showInfo "Installed Ghidra"
 
 # Update root user environment
 showInfo "Configuring root environment"
 checkMakeDir /root/.tmux
 checkMakeDir /root/.tmux/plugins
-wget -O /root/.bashrc https://raw.githubusercontent.com/hermit-hacker/ctf-tools/master/root/.bashrc >> chwhb.log 2>&1
-wget -O /root/.tmux.conf https://raw.githubusercontent.com/hermit-hacker/ctf-tools/master/root/.tmux.conf >> chwhb.log 2>&1
-wget -O /root/.bash_aliases https://raw.githubusercontent.com/hermit-hacker/ctf-tools/master/.bash_aliases >> chwhb.log 2>&1
+wget -O /root/.bashrc https://raw.githubusercontent.com/hermit-hacker/ctf-tools/master/root/.bashrc >> ${LOGFILE} 2>&1
+wget -O /root/.tmux.conf https://raw.githubusercontent.com/hermit-hacker/ctf-tools/master/root/.tmux.conf >> ${LOGFILE} 2>&1
+wget -O /root/.bash_aliases https://raw.githubusercontent.com/hermit-hacker/ctf-tools/master/.bash_aliases >> ${LOGFILE} 2>&1
+
+# Grab some of Hermit's Helpful Helper Scripts from the CTF-Tools repo
 showInfo "Pulling down helper files from Hermit's GitHub"
-wget -O /usr/local/bin/ByteAdder.py https://raw.githubusercontent.com/hermit-hacker/ctf-tools/master/ByteAdder.py >> chwhb.log 2>&1
+wget -O /usr/local/bin/ByteAdder.py https://raw.githubusercontent.com/hermit-hacker/ctf-tools/master/ByteAdder.py >> ${LOGFILE} 2>&1
 chmod +x /usr/local/bin/ByteAdder.py
-wget -O /usr/local/bin/apk-process.sh https://raw.githubusercontent.com/hermit-hacker/ctf-tools/master/apk-process.sh >> chwhb.log 2>&1
+wget -O /usr/local/bin/apk-process.sh https://raw.githubusercontent.com/hermit-hacker/ctf-tools/master/apk-process.sh >> ${LOGFILE} 2>&1
 chmod +x /usr/local/bin/apk-process.sh
-wget -O /usr/local/bin/ascii2bin.sh https://raw.githubusercontent.com/hermit-hacker/ctf-tools/master/ascii-to-bin.sh >> chwhb.log 2>&1
+wget -O /usr/local/bin/ascii2bin.sh https://raw.githubusercontent.com/hermit-hacker/ctf-tools/master/ascii-to-bin.sh >> ${LOGFILE} 2>&1
 chmod +x /usr/local/bin/ascii2bin.sh
-wget -O /usr/local/bin/rotall.sh https://raw.githubusercontent.com/hermit-hacker/ctf-tools/master/rot-all.sh >> chwhb.log 2>&1
+wget -O /usr/local/bin/rotall.sh https://raw.githubusercontent.com/hermit-hacker/ctf-tools/master/rot-all.sh >> ${LOGFILE} 2>&1
 chmod +x /usr/local/bin/rotall.sh
-wget -O /usr/local/bin/tarsum.py https://raw.githubusercontent.com/hermit-hacker/ctf-tools/master/tarsum.py >> chwhb.log 2>&1
+wget -O /usr/local/bin/tarsum.py https://raw.githubusercontent.com/hermit-hacker/ctf-tools/master/tarsum.py >> ${LOGFILE} 2>&1
 chmod +x /usr/local/bin/tarsum.py
-wget -O /usr/local/bin/wgm.sh https://raw.githubusercontent.com/hermit-hacker/ctf-tools/master/wgm.sh >> chwhb.log 2>&1
+wget -O /usr/local/bin/wgm.sh https://raw.githubusercontent.com/hermit-hacker/ctf-tools/master/wgm.sh >> ${LOGFILE} 2>&1
 chmod +x /usr/local/bin/wgm.sh
+
+# Add Tmux customizations
 purgeIfFound /root/.tmux/plugins/tpm
-git clone https://github.com/tmux-plugins/tpm /root/.tmux/plugins/tpm >> chwhb.log 2>&1
+git clone https://github.com/tmux-plugins/tpm /root/.tmux/plugins/tpm >> ${LOGFILE} 2>&1
 purgeIfFound /root/.tmux/plugins/tmux-logging
-git clone https://github.com/tmux-plugins/tmux-logging /root/.tmux/plugins/tmux-logging >> chwhb.log 2>&1
+git clone https://github.com/tmux-plugins/tmux-logging /root/.tmux/plugins/tmux-logging >> ${LOGFILE} 2>&1
 purgeIfFound /root/OSCP-Notes
-git clone https://github.com/hermit-hacker/OSCP-Study-Notes.git /root/OSCP-Notes >> chwhb.log 2>&1
+git clone https://github.com/hermit-hacker/OSCP-Study-Notes.git /root/OSCP-Notes >> ${LOGFILE} 2>&1
 showInfo "Installations complete!"
 
 showInfo "Cleaning up unused packages"
-apt -y -q autoremove >> chwhb.log 2>&1
-showInfo "Clean up complete!"
+apt -y -q autoremove >> >> ${LOGFILE} 2>&1
+unset ${DEBIAN_FRONTEND}
+unset ${OSNAME}
+unset ${LOGFILE}
+unset ${GTEMP}
+unset ${GINSTALLDIR}
+unset ${GHIDRAFILE}
+unset ${SALAEFILE}
+unset ${DISCORDTMPDIR}
 
+showInfo "Clean up complete!"
+showInfo "--------------------------------------------"
 showInfo "Installation tasks completed, exiting CHWHB!"
+echo
